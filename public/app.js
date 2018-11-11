91c285f5eeaf922aeaa5e414d2d709acf31c6f94
srandle93 <randle.symone.l@gmail.com>
2018-11-10 21:21:46 -0600

    git deployment 11-10-18.

--- public/app.js
+++ public/app.js
@@ -1,49 +1,51 @@
-$.getJSON("/articles", data => {
-  // For each one
-  for (var i = 0; i < data.length; i++) {
-    // Display the apropos information on the page
-    $(".data").append(`${data[i].title} ${"<br>"} ${data[i].link}`);
-  }
-});
-
-// Click events home, savedArticles, scrapeArticles, clearArticles, saveArt
-$("#home").on("click", () => {
-  res.send('index', {
+$(document).ready( () => {
+  $.getJSON("/articles", (data) => {
+    // For each one
+    for (var i = 0; i < data.length; i++) {
+      // Display the apropos information on the page
+      $(".data").append(`${data[i].title} ${"<br>"} ${data[i].link}`);
+    }
   });
-});
 
-$("#savedArticles").on("click", () => {
-  $.ajax({
-    method: "POST",
-    url: `/saved/`,
-    data: data
-  })
-  res.send(data);
-});
+  // Click events home, savedArticles, scrapeArticles, clearArticles, saveArt
+  $("#home").on("click", () => {
+    res.send('index', {
+    });
+  });
 
-$("#scrapeArticles").on("click", () => {
-  $.ajax({
-    method: "POST",
-    url: "/articles",
-    data: data
-  })
-  res.render("/scrape");
-});
+  $("#savedArticles").on("click", () => {
+    $.ajax({
+      method: "POST",
+      url: `/saved/`,
+      data: data
+    })
+    res.send(data);
+  });
 
-$("#saveArt").on("click", () => {
-  $.ajax({
-    method: "UPDATE",
-    url: "/articles",
-    data: data
-  })
-  res.render(data[i]);
-});
-$("#clearArticles").on("click", () => {
-  $.ajax({
-    method: "DELETE",
-    url: "/",
-    data: data
-  })
-  res.render(data.empty());
-});
+  $("#scrapeArticles").on("click", () => {
+    $.ajax({
+      method: "POST",
+      url: "/articles",
+      data: data
+    })
+    res.render("/scrape");
+  });
 
+  $("#saveArt").on("click", () => {
+    $.ajax({
+      method: "UPDATE",
+      url: "/articles",
+      data: data
+    })
+    res.render(data[i]);
+  });
+  $("#clearArticles").on("click", () => {
+    $.ajax({
+      method: "DELETE",
+      url: "/",
+      data: data
+    })
+    res.render(data.empty());
+  });
+
+});
